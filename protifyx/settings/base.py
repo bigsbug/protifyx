9dc4b5966e4447b277dc14851cb87fe012514430
bigsbug <booxgitboox@gmail.com>
2023-04-13 14:29:33 +0330

    create and register custom User model

--- protifyx/settings/base.py
+++ protifyx/settings/base.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Define A New User Model To Django
+AUTH_USER_MODEL = "account.user"
