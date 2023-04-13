cbf3d7b3ae5377603ac24a63a363182a905ba59c
bigsbug <booxgitboox@gmail.com>
2023-04-13 13:27:03 +0330

    re structure settings of project

---
+++ protifyx/settings/dev.py
@@ -0,0 +1,6 @@
+"""Development settings """
+
+from protifyx.settings.base import *
+
+# install custom apps for development env
+INSTALLED_APPS += []
---
+++ protifyx/settings/production.py
@@ -0,0 +1,6 @@
+"""Production settings """
+
+from protifyx.settings.base import *
+
+# install custom apps for production env
+INSTALLED_APPS += []
